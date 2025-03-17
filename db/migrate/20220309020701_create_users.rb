1fe34f0709a9a1c1b83c0cfb63701de89010c1b7
nishant kumar <nishant.kumar@kellogg.northwestern.edu>
2025-03-17 03:17:53 +0000

    Fixed migration error for unique email constraint

--- db/migrate/20220309020701_create_users.rb
+++ db/migrate/20220309020701_create_users.rb
@@ -1,11 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-      t.string "name"
-      t.string "email", null: false, unique: true
-      t.string "password_digest"  
+      t.string :email, null: false
+      t.string :password_digest, null: false
 
       t.timestamps
     end
+
+    add_index :users, :email, unique: true
   end
 end
