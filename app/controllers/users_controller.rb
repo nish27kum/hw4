ad4782a78019c51909a4f4afa4a06e5d2149c128
nishant kumar <nishant.kumar@kellogg.northwestern.edu>
2025-03-17 04:18:56 +0000

    hw4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,18 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new 
+    @user = User.new  
   end
 
   def create
-    if params[:user].present?  # Ensure `user` key exists before requiring params
-      @user = User.new(user_params)
+    @user = User.new(user_params)  
 
-      if @user.save
-        session[:user_id] = @user.id
-        flash[:notice] = "Welcome, #{@user.name}! Your account has been created successfully!"
-        redirect_to "/places"
-      else
-        flash[:alert] = "Signup failed! Please check your details and try again."
-        render :new
-      end
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome, #{@user.name}! Your account has been created successfully!"
+      redirect_to "/places"
     else
-      flash[:alert] = "Signup failed! Missing required parameters."
-      redirect_to "/signup"
+      flash[:alert] = "Signup failed! Please check your details."
+      render :new
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_17_002102) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_17_041541) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -51,18 +51,18 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_17_002102) do
 
   create_table "places", force: :cascade do |t|
     t.string "name"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
+    t.string "name", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
